6c629067d855a1175c0d7c4b3a28da7cddccc868
Thiago José da Cruz <tj3137488@gmail.com>
2025-05-05 17:44:29 -0300

    atividades de C

---
+++ 1CursoC/atividades/atividade61.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+    float n1, n2;
+
+    scanf("%f", &n1);
+    scanf("%f", &n2);
+
+    if
+            (n1 < n2) {
+        printf("%0.2f", n1);
+
+    } else {
+        printf("%0.2f ", n2);
+
+    }
+    return 0;
+
+
+}
