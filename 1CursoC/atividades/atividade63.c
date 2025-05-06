fe50ccefe0365b54dc07b476b1d9d5aa23516878
Thiago José da Cruz <tj3137488@gmail.com>
2025-05-06 13:31:21 -0300

    c

---
+++ 1CursoC/atividades/atividade63.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int var = 15;
+
+    printf("conteudo de var = %d\n", var);
+    printf("endereço de var = %p\n", &var)
+    
+
+
+    return 0;
+
+}
