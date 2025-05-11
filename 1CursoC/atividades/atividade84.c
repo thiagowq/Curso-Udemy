8dc861996107d746e88c84c9c61af852f784e7c6
Thiago José da Cruz <tj3137488@gmail.com>
2025-05-11 15:15:49 -0300

    atividades de C

--- 1CursoC/atividades/atividade84.c
+++ 1CursoC/atividades/atividade84.c
@@ -2,9 +2,23 @@
 #include <stdlib.h>
 
 int main(){
-
     
+    int numeros = 1, i;
 
+    printf("Digite 10 numeros:");
+    scanf("%d", &i);
+
+
+    for (numeros = 1; numeros < 10; numeros++)
+    {
+        printf("Digite 10 numeros:");
+        scanf("%d", &i);
+
+        
+
+    }
+    
+    printf("%d", i);
 
 
     return 0;
