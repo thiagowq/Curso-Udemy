bc7b42f1bb4c77358bee8a6c75791b88a8437379
Thiago José da Cruz <tj3137488@gmail.com>
2025-06-03 18:08:13 -0300

    Update atividade152.c

--- 1CursoC/atividades/atividade152.c
+++ 1CursoC/atividades/atividade152.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+void infoPaciente (int ID[], int idade[], float peso[]){
+    for (int i = 0; i < 100; i++){
+        printf("Digite o ID do paciente:");
+        scanf("%d", &ID[i]);
+
+        if (ID[i] == 0){
+            break;
+        }
+        
+        printf("Digite a idade do paciente:\n");
+        scanf("%d", &idade);
+
+        printf("Digite o peso do paciente:\n");
+        scanf("%f", &peso);
+    }
+    
+}
+
+void (){
+
+} 
+
 
 int main(){
+
+
+
+    infoPaciente(ID, idade, peso);
 
     return 0;
 }
